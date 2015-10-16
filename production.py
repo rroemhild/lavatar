97bfee5e19aca2ad055cea963f1dfe8bc5517629
Rafael Römhild <rafael@roemhild.de>
2015-10-17 00:26:15 +0200

    Use gevent.monkey.patch_all()

--- production.py
+++ production.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from gevent import monkey; monkey.patch_all()
+
 import os
 
 if 'LAVATAR_SETTINGS' not in os.environ:
     config = 'settings.py'
     os.environ['LAVATAR_SETTINGS'] = os.path.abspath(config)
 
-from gevent.wsgi import WSGIServer
+from gevent.pywsgi import WSGIServer
 from lavatar import app
 
-if not app.debug:
-    import logging
-    handler = logging.StreamHandler()
-    log = logging.getLogger('werkzeug')
-    log.setLevel(logging.WARNING)
-    log.addHandler(handler)
 
 host = os.environ.get('APP_HOST', '127.0.0.1')
 port = os.environ.get('APP_PORT', 5000)
